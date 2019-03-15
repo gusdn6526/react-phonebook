b29301d63ffda0665a7f0908da97cb6e593f8afe
gusdn6526 <gusdn6526@gmail.com>
2019-03-15 17:37:16 +0900

    Practice react

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
 import React, { Component } from 'react'; // JSX 를 사용하려면, 꼭 React 를 import 해주어야 합니다.
 import logo from './logo.svg';
 import './App.css';
+import MyName from './MyName'
+import Counter from './Counter'
 
 //이렇게, import 를 하는 것은, 우리가 webpack 을 사용하기에 가능한 작업입니다.
 
 class App extends Component {
   render() {
+    const name = "react";
+    const style = {
+      backgroundColor: 'black',
+      padding: '16px',
+      color: 'white',
+      fontSize: '12px'
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <div style = {style}>
+          Hello {name}!
+          {
+            1+1 === 2 ? (<div>O</div>) : (<div>X</div>)
+          }
+        </div>
+
+        <div className="App">
+          리액트
+        </div>
+
+        <div>
+          <MyName name = 'moosekiss' />
+        </div>
+
+        <div>
+          <Counter />
+        </div>
+
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+/*
+클래스형태로 만들어진 컴포넌트에는 꼭, render 함수가 있어야 하구요,
+그리고 그 내부에서는 JSX 를 return 해주어야 합니다.
+위에 보이는 HTML 같은 코드가 바로, JSX 입니다.
+*/
+
+export default App; //우리가 작성한 컴포넌트를 다른 곳에서 불러와서 사용 할 수 있도록 내보내기를 해줍니다.
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App';  // 우리가 만든 컴포넌트를 불러올 때는 이렇게 import 를 사용해서 불러와줍니다
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+/*
+그리고 브라우저 상에 우리의 리액트 컴포넌트를 보여주기 위해서는 ReactDOM.render 함수를 사용합니다.
+첫번째 파라미터는 렌더링 할 결과물이고, 
+두번째 파라미터는 컴포넌트를 어떤 DOM 에 그릴지 정해줍니다.
+*/
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
